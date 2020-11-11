126360253d9b34571c75e3e4940913238336d4ec
MasatoMakino <unagiinu.g@gmail.com>
2020-11-11 17:44:51 +0900

    add : test for selenium #3
    ChromeDriver用のみ。

---
+++ selenium/test.js
@@ -0,0 +1,44 @@
+const { Builder, Capabilities, logging } = require("selenium-webdriver");
+
+const pref = new logging.Preferences();
+pref.setLevel(logging.Type.BROWSER, logging.Level.DEBUG);
+const capabilities = Capabilities.chrome();
+capabilities.setLoggingPrefs(pref);
+
+const testImageLoad = async () => {
+  const driver = await new Builder().withCapabilities(capabilities).build();
+  try {
+    await driver.get(
+      "https://masatomakino.github.io/pixijs-loader-util/demo/demo_fetchImage.html"
+    );
+    await onLoadedImageTest(driver);
+
+    await driver.navigate().to("https://www.google.com/");
+
+    await driver.navigate().back();
+    await onLoadedImageTest(driver);
+  } catch (e) {
+    console.log(e);
+  } finally {
+    // Chromeを終了
+    driver && (await driver.quit());
+  }
+};
+
+const onLoadedImageTest = async (driver) => {
+  driver.wait(driver.executeScript("return window.onFetch"), 10000);
+  const posX = await driver.executeScript("return window.posX");
+  const posY = await driver.executeScript("return window.posY");
+  if (posX !== 325 || posY !== 225) {
+    console.warn(posX, posY);
+  }
+};
+
+const outputLog = async (driver) => {
+  const entries = await driver.manage().logs().get(logging.Type.BROWSER);
+  entries.forEach((entry) => {
+    console.log("[%s] %s", entry.level.name, entry.message);
+  });
+};
+
+testImageLoad();
