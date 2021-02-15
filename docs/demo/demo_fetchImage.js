fdcc169e4b9ea60a0aa804952a02714d45f5f92d
MasatoMakino <unagiinu.g@gmail.com>
2021-02-15 17:20:49 +0900

    update : docs and bin.

--- docs/demo/demo_fetchImage.js
+++ docs/demo/demo_fetchImage.js
@@ -1,6 +1,6 @@
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is not neither made for production nor for readable output files.
+ * This devtool is neither made for production nor for readable output files.
  * It uses "eval()" calls to create a separate source file in the browser devtools.
  * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
  * or disable the default devtool with "devtool: false".
@@ -14,14 +14,9 @@
 /*!***************************!*\
   !*** ./demoSrc/common.js ***!
   \***************************/
-/*! namespace exports */
-/*! export initApp [provided] [no usage info] [missing usage info prevents renaming] */
-/*! export initLink [provided] [no usage info] [missing usage info prevents renaming] */
-/*! other exports [not provided] [no usage info] */
-/*! runtime requirements: __webpack_require__, __webpack_require__.r, __webpack_exports__, __webpack_require__.d, __webpack_require__.* */
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"initApp\": () => /* binding */ initApp,\n/* harmony export */   \"initLink\": () => /* binding */ initLink\n/* harmony export */ });\n/* harmony import */ var pixi_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! pixi.js */ \"./node_modules/pixi.js/lib/pixi.es.js\");\n\nfunction initApp(w = 800, h = 600) {\n  const app = new pixi_js__WEBPACK_IMPORTED_MODULE_0__.Application({\n    width: w,\n    height: h\n  });\n  document.body.appendChild(app.view);\n  return app;\n}\nfunction initLink() {\n  const link = document.createElement(\"a\");\n  link.href = \"https://google.com\";\n  link.innerHTML = \"jump to google\";\n  const div = document.createElement(\"div\");\n  div.appendChild(link);\n  document.body.appendChild(div);\n}\n\n//# sourceURL=webpack://pixijs-loader-util/./demoSrc/common.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"initApp\": () => (/* binding */ initApp),\n/* harmony export */   \"initLink\": () => (/* binding */ initLink)\n/* harmony export */ });\n/* harmony import */ var pixi_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! pixi.js */ \"./node_modules/pixi.js/lib/pixi.es.js\");\n\nfunction initApp(w = 800, h = 600) {\n  const app = new pixi_js__WEBPACK_IMPORTED_MODULE_0__.Application({\n    width: w,\n    height: h\n  });\n  document.body.appendChild(app.view);\n  return app;\n}\nfunction initLink() {\n  const link = document.createElement(\"a\");\n  link.href = \"https://google.com\";\n  link.innerHTML = \"jump to google\";\n  const div = document.createElement(\"div\");\n  div.appendChild(link);\n  document.body.appendChild(div);\n}\n\n//# sourceURL=webpack://pixijs-loader-util/./demoSrc/common.js?");
 
 /***/ }),
 
@@ -29,9 +24,6 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /*!************************************!*\
   !*** ./demoSrc/demo_fetchImage.js ***!
   \************************************/
-/*! namespace exports */
-/*! exports [not provided] [no usage info] */
-/*! runtime requirements: __webpack_require__, __webpack_require__.n, __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var pixi_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! pixi.js */ \"./node_modules/pixi.js/lib/pixi.es.js\");\n/* harmony import */ var _common__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./common */ \"./demoSrc/common.js\");\n/* harmony import */ var _lib__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../lib */ \"./lib/index.js\");\n/* harmony import */ var _lib__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_lib__WEBPACK_IMPORTED_MODULE_2__);\n\n\n\nwindow.onFetch = false;\nwindow.posX = 0;\nwindow.posY = 0;\n\nconst onDomContentsLoaded = () => {\n  const W = 800;\n  const H = 600;\n  const app = (0,_common__WEBPACK_IMPORTED_MODULE_1__.initApp)(W, H);\n  const img = pixi_js__WEBPACK_IMPORTED_MODULE_0__.Sprite.from(\"./150.png\");\n  app.stage.addChild(img);\n  (0,_lib__WEBPACK_IMPORTED_MODULE_2__.fetchImage)(img).then(sprite => {\n    const baseTexture = sprite.texture.baseTexture;\n    sprite.x = window.posX = W / 2 - baseTexture.width / 2;\n    sprite.y = window.posY = H / 2 - baseTexture.height / 2;\n    window.onFetch = true;\n  });\n  (0,_common__WEBPACK_IMPORTED_MODULE_1__.initLink)();\n};\n\nif (document.readyState !== \"loading\") {\n  onDomContentsLoaded();\n} else {\n  document.addEventListener(\"DOMContentLoaded\", onDomContentsLoaded);\n}\n\n//# sourceURL=webpack://pixijs-loader-util/./demoSrc/demo_fetchImage.js?");
@@ -42,11 +34,6 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var pixi
 /*!******************************!*\
   !*** ./lib/ImageLoadUtil.js ***!
   \******************************/
-/*! flagged exports */
-/*! export __esModule [provided] [no usage info] [missing usage info prevents renaming] */
-/*! export fetchImage [provided] [no usage info] [missing usage info prevents renaming] */
-/*! other exports [not provided] [no usage info] */
-/*! runtime requirements: __webpack_exports__ */
 /***/ ((__unused_webpack_module, exports) => {
 
 eval("\n\nObject.defineProperty(exports, \"__esModule\", ({\n  value: true\n}));\nexports.fetchImage = void 0;\n/**\n * Sprite.from(\"img\")で生成されたSpriteの、画像ロード後の処理を指定する。\n * テクスチャバッファに保存されているか否かにかかわらず、実行されることを保証する。\n *\n * @param sprite\n */\n\nfunction fetchImage(sprite) {\n  return new Promise(function (resolve, reject) {\n    if (sprite.texture.baseTexture.width !== 0) {\n      resolve(sprite);\n    } else {\n      sprite.texture.baseTexture.once(\"loaded\", function () {\n        resolve(sprite);\n      });\n    }\n  });\n}\n\nexports.fetchImage = fetchImage;\n\n//# sourceURL=webpack://pixijs-loader-util/./lib/ImageLoadUtil.js?");
@@ -57,11 +44,6 @@ eval("\n\nObject.defineProperty(exports, \"__esModule\", ({\n  value: true\n}));
 /*!******************************!*\
   !*** ./lib/MovieLoadUtil.js ***!
   \******************************/
-/*! flagged exports */
-/*! export __esModule [provided] [no usage info] [missing usage info prevents renaming] */
-/*! export loadAutoPlayableMovie [provided] [no usage info] [missing usage info prevents renaming] */
-/*! other exports [not provided] [no usage info] */
-/*! runtime requirements: __webpack_exports__ */
 /***/ ((__unused_webpack_module, exports) => {
 
 eval("\n\nObject.defineProperty(exports, \"__esModule\", ({\n  value: true\n}));\nexports.loadAutoPlayableMovie = void 0;\n/**\n * PixiJSのTextureとして扱える、autoplay可能なVideoElementを取得する。\n * VideoElementには、iOSおよびChromeのautoplayポリシーを回避するための設定がされている。\n *\n * @param url アセットのURL\n */\n\nfunction loadAutoPlayableMovie(url) {\n  var video = document.createElement(\"video\");\n  video.src = url;\n  video.muted = true;\n  video.setAttribute(\"playsinline\", \"\");\n  return video;\n}\n\nexports.loadAutoPlayableMovie = loadAutoPlayableMovie;\n\n//# sourceURL=webpack://pixijs-loader-util/./lib/MovieLoadUtil.js?");
@@ -72,12 +54,6 @@ eval("\n\nObject.defineProperty(exports, \"__esModule\", ({\n  value: true\n}));
 /*!**********************!*\
   !*** ./lib/index.js ***!
   \**********************/
-/*! unknown exports (runtime-defined) */
-/*! runtime requirements: top-level-this-exports, __webpack_exports__, __webpack_require__ */
-/*! CommonJS bailout: this is used directly at 3:22-26 */
-/*! CommonJS bailout: this is used directly at 16:19-23 */
-/*! CommonJS bailout: exports is used directly at 24:41-48 */
-/*! CommonJS bailout: exports is used directly at 26:41-48 */
 /***/ (function(__unused_webpack_module, exports, __webpack_require__) {
 
 eval("\n\nvar __createBinding = this && this.__createBinding || (Object.create ? function (o, m, k, k2) {\n  if (k2 === undefined) k2 = k;\n  Object.defineProperty(o, k2, {\n    enumerable: true,\n    get: function () {\n      return m[k];\n    }\n  });\n} : function (o, m, k, k2) {\n  if (k2 === undefined) k2 = k;\n  o[k2] = m[k];\n});\n\nvar __exportStar = this && this.__exportStar || function (m, exports) {\n  for (var p in m) if (p !== \"default\" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);\n};\n\nObject.defineProperty(exports, \"__esModule\", ({\n  value: true\n}));\n\n__exportStar(__webpack_require__(/*! ./ImageLoadUtil */ \"./lib/ImageLoadUtil.js\"), exports);\n\n__exportStar(__webpack_require__(/*! ./MovieLoadUtil */ \"./lib/MovieLoadUtil.js\"), exports);\n\n//# sourceURL=webpack://pixijs-loader-util/./lib/index.js?");
@@ -115,14 +91,17 @@ eval("\n\nvar __createBinding = this && this.__createBinding || (Object.create ?
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
+/******/ 	// the startup function
+/******/ 	// It's empty as some runtime module handles the default behavior
+/******/ 	__webpack_require__.x = x => {};
 /************************************************************************/
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
 /******/ 		__webpack_require__.n = (module) => {
 /******/ 			var getter = module && module.__esModule ?
-/******/ 				() => module['default'] :
-/******/ 				() => module;
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
 /******/ 			__webpack_require__.d(getter, { a: getter });
 /******/ 			return getter;
 /******/ 		};
@@ -154,7 +133,7 @@ eval("\n\nvar __createBinding = this && this.__createBinding || (Object.create ?
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
@@ -201,43 +180,10 @@ eval("\n\nvar __createBinding = this && this.__createBinding || (Object.create ?
 /******/ 		
 /******/ 		// no HMR manifest
 /******/ 		
-/******/ 		var checkDeferredModules = () => {
-/******/ 		
-/******/ 		};
-/******/ 		function checkDeferredModulesImpl() {
-/******/ 			var result;
-/******/ 			for(var i = 0; i < deferredModules.length; i++) {
-/******/ 				var deferredModule = deferredModules[i];
-/******/ 				var fulfilled = true;
-/******/ 				for(var j = 1; j < deferredModule.length; j++) {
-/******/ 					var depId = deferredModule[j];
-/******/ 					if(installedChunks[depId] !== 0) fulfilled = false;
-/******/ 				}
-/******/ 				if(fulfilled) {
-/******/ 					deferredModules.splice(i--, 1);
-/******/ 					result = __webpack_require__(__webpack_require__.s = deferredModule[0]);
-/******/ 				}
-/******/ 			}
-/******/ 			if(deferredModules.length === 0) {
-/******/ 				__webpack_require__.x();
-/******/ 				__webpack_require__.x = () => {
-/******/ 		
-/******/ 				}
-/******/ 			}
-/******/ 			return result;
-/******/ 		}
-/******/ 		__webpack_require__.x = () => {
-/******/ 			// reset startup function so it can be called again when more startup code is added
-/******/ 			__webpack_require__.x = () => {
-/******/ 		
-/******/ 			}
-/******/ 			chunkLoadingGlobal = chunkLoadingGlobal.slice();
-/******/ 			for(var i = 0; i < chunkLoadingGlobal.length; i++) webpackJsonpCallback(chunkLoadingGlobal[i]);
-/******/ 			return (checkDeferredModules = checkDeferredModulesImpl)();
-/******/ 		};
+/******/ 		var checkDeferredModules = x => {};
 /******/ 		
 /******/ 		// install a JSONP callback for chunk loading
-/******/ 		var webpackJsonpCallback = (data) => {
+/******/ 		var webpackJsonpCallback = (parentChunkLoadingFunction, data) => {
 /******/ 			var [chunkIds, moreModules, runtime, executeModules] = data;
 /******/ 			// add "moreModules" to the modules object,
 /******/ 			// then flag all "chunkIds" as loaded and fire callback
@@ -255,7 +201,7 @@ eval("\n\nvar __createBinding = this && this.__createBinding || (Object.create ?
 /******/ 				}
 /******/ 			}
 /******/ 			if(runtime) runtime(__webpack_require__);
-/******/ 			parentChunkLoadingFunction(data);
+/******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
 /******/ 			while(resolves.length) {
 /******/ 				resolves.shift()();
 /******/ 			}
@@ -268,12 +214,39 @@ eval("\n\nvar __createBinding = this && this.__createBinding || (Object.create ?
 /******/ 		}
 /******/ 		
 /******/ 		var chunkLoadingGlobal = self["webpackChunkpixijs_loader_util"] = self["webpackChunkpixijs_loader_util"] || [];
-/******/ 		var parentChunkLoadingFunction = chunkLoadingGlobal.push.bind(chunkLoadingGlobal);
-/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback;
+/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
+/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 		
+/******/ 		function checkDeferredModulesImpl() {
+/******/ 			var result;
+/******/ 			for(var i = 0; i < deferredModules.length; i++) {
+/******/ 				var deferredModule = deferredModules[i];
+/******/ 				var fulfilled = true;
+/******/ 				for(var j = 1; j < deferredModule.length; j++) {
+/******/ 					var depId = deferredModule[j];
+/******/ 					if(installedChunks[depId] !== 0) fulfilled = false;
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferredModules.splice(i--, 1);
+/******/ 					result = __webpack_require__(__webpack_require__.s = deferredModule[0]);
+/******/ 				}
+/******/ 			}
+/******/ 			if(deferredModules.length === 0) {
+/******/ 				__webpack_require__.x();
+/******/ 				__webpack_require__.x = x => {};
+/******/ 			}
+/******/ 			return result;
+/******/ 		}
+/******/ 		var startup = __webpack_require__.x;
+/******/ 		__webpack_require__.x = () => {
+/******/ 			// reset startup function so it can be called again when more startup code is added
+/******/ 			__webpack_require__.x = startup || (x => {});
+/******/ 			return (checkDeferredModules = checkDeferredModulesImpl)();
+/******/ 		};
 /******/ 	})();
 /******/ 	
 /************************************************************************/
 /******/ 	// run startup
-/******/ 	return __webpack_require__.x();
+/******/ 	__webpack_require__.x();
 /******/ })()
 ;
