a117cd70dbbbc2260505d67434e6de2445fb0f35
MasatoMakino <makino.masato.g@gmail.com>
2023-12-19 16:10:08 +0900

    add : loadAutoPlayableMovie
    インスタンス化のみを単体テストする。video要素の操作はjsdomではサポートされていないため。

---
+++ __test__/MovieLoadUtil.spec.ts
@@ -0,0 +1,11 @@
+import { loadAutoPlayableMovie } from "../src/MovieLoadUtil";
+import { describe, test, expect } from "vitest";
+
+describe("MovieLoadUtil", () => {
+  test("Load movie", async () => {
+    const video = loadAutoPlayableMovie(
+      "demoSrc/severe-storm-over-mandurah.mp4"
+    );
+    expect(video).toBeTruthy();
+  });
+});
