466dc1a8024d22001aa99b77414183c4210ebdbe
MasatoMakino <makino.masato.g@gmail.com>
2024-03-07 14:20:17 +0900

    fix : unit tests

--- __test__/ImageLoadUtil.spec.ts
+++ __test__/ImageLoadUtil.spec.ts
@@ -1,53 +1,11 @@
-import { fetchImage } from "../src/ImageLoadUtil.js";
 import { describe, test, expect } from "vitest";
-import { Sprite } from "pixi.js";
+import { Assets, Sprite } from "pixi.js";
 import { TestImage } from "./TestImage.js";
 
 describe("ImageLoadUtil", () => {
-  const onLoadImage = async (sprite: Sprite): Promise<void> => {
-    await new Promise<void>((resolve) => {
-      sprite.texture.baseTexture.once("loaded", () => {
-        expect(sprite.texture.baseTexture.width).not.toBe(0);
-        resolve();
-      });
-      sprite.texture.baseTexture.once("update", () => {
-        expect(sprite.texture.baseTexture.width).not.toBe(0);
-        resolve();
-      });
-      sprite.texture.baseTexture.once("error", (e) => {
-        throw e;
-      });
-    });
-  };
-
   test("Load image", async () => {
+    await Assets.load(TestImage);
     const sprite = Sprite.from(TestImage);
-    expect(sprite.texture.baseTexture.width).toBe(0);
-
-    const promise = fetchImage(sprite);
-    await onLoadImage(sprite);
-    const loadedImage = (await promise) as Sprite;
-    expect(loadedImage.texture.baseTexture.width).not.toBe(0);
-  }, 500);
-
-  test("Image loading sets texture size after the first load", () => {
-    const sprite = Sprite.from(TestImage);
-    expect(sprite.texture.baseTexture.width).not.toBe(0);
-  });
-
-  test.fails(
-    "Repeated image loading does not call loaded callback",
-    async () => {
-      const sprite = Sprite.from(TestImage);
-      await onLoadImage(sprite);
-    },
-    300,
-  );
-
-  test("Ensures load completion even for cached images", async () => {
-    const sprite = Sprite.from(TestImage);
-
-    const loadedImage = (await fetchImage(sprite)) as Sprite;
-    expect(loadedImage.texture.baseTexture.width).not.toBe(0);
+    expect(sprite.width).toBe(8);
   }, 500);
 });
